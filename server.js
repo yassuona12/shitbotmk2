d4dac37cc0b70a74a6cc46ccd99320d6d11ce206
Glitch (hello-express) <none>
2020-09-06 15:54:12 +0000

    🚝🎺 Checkpoint ./server.js:9957624/496

--- server.js
+++ server.js
@@ -73,9 +73,11 @@ bot.on("message", async message => {
 
 //logging stuff *not completed yeet
 bot.on("messageDelete", message => {
+  let njir = message.mentions
   const deleted = new Discord.RichEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL)
-    .addField(`Mesaage sent by ${message.author.username.tag} deleted in <#${message.channel.id}>`, message.content)
+    .setTittle(`message sent by <@${message.author.id}> deleted in <#${message.channel.id}>`)
+    .addField("", message.content)
     .setTimestamp()
     .setFooter("log", message.guild.iconURL);
   let logchan = message.guild.channels.find( channel => channel.name ==="log");
