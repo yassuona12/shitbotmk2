fe2771dd3ab7abab07915daa693dfd30d18fada8
Glitch (hello-express) <none>
2020-09-06 14:55:48 +0000

    👝🕴 Checkpoint ./prefixes.json:9957624/6 ./commands/ping.js:9957624/6

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports.run = (bot, message) => {
   let embed = new Discord.RichEmbed()
 
-   .setDescription(`馃彄Pong! It took **${Math.abs(Date.now() - message.createdTimestamp)}**ms`);
+   .setDescription(`🛑Pong! It took **${Math.abs(Date.now() - message.createdTimestamp)}**ms`);
 
   message.channel.send({embed: embed})
 };
