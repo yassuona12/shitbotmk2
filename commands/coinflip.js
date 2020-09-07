84727703174a66309d0261b49179d163e0162aeb
Glitch (five-tricky-cardboard) <none>
2020-09-07 06:55:07 +0000

    🏘💖 Checkpoint ./commands/coinflip.js:21161892/1993

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,17 +1,19 @@
+const Discord = require('discord.js')
 exports.run = async (bot, message, args) => {
   let random = (Math.floor(Math.random() * Math.floor(2)));
-
+  const user = message.author
         if(random === 0) {
-          message.channel.send(`I flipped heads!${files = ["https://cdn.discordapp.com/attachments/454680816061120513/689019575592943626/IMG_20200316_155631.jpg"] }`);
+          const heads = new Discord.RichEmbed()
+          .setAuthor(`**${user.tag} I Flipped Heads**`)
+          .setImage("https://cdn.discordapp.com/attachments/454680816061120513/689019575592943626/IMG_20200316_155631.jpg")
+          message.channel.send(heads);
         }
         else {
-          message.channel.send('I flipped tails!');
+          const tails = new Discord.RichEmbed()
+          .setAuthor(`**${user.tag} I Flipped Tails**`)
+          .setImage("https://cdn.discordapp.com/attachments/454680816061120513/689019762529140765/IMG_20200316_155650.jpg")
+          message.channel.send(tails);
         }
-      message.channel.send({files : ["https://cdn.discordapp.com/attachments/454680816061120513/689019575592943626/IMG_20200316_155631.jpg"] });
-    }
-    else {
-      message.channel.send({files : ["https://cdn.discordapp.com/attachments/454680816061120513/689019762529140765/IMG_20200316_155650.jpg"] });
-    }
 },
 
 exports.conf = {
@@ -25,4 +27,4 @@ exports.help = {
     name: 'coinflip',
     description: 'Flip a Coin',
     usage: 'coinflip'
-  };h
+  };
