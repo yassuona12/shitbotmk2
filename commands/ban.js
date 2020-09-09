57088cde1331d553470cc6c8e1cc7dd2080098bb
Glitch (five-tricky-cardboard) <none>
2020-09-09 03:39:21 +0000

    👱🎍 Checkpoint ./commands/ban.js:21161892/246

--- commands/ban.js
+++ commands/ban.js
@@ -8,12 +8,12 @@ module.exports.run = async (bot, message, args) => {
       message.reply("Usage: !ban <user> <reason>");
       return;
     }
-    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-    if(!bUser) return ;
-    if(bUser.id === bot.user.id) return message.channel.send('Member tidak ditemukan');
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author);
+    if(!bUser) return message.channel.send('Member tidak ditemukan');
+    if(bUser.id === bot.user.id) return message.channel.send('Member tidak bisa membanned diri sendiri');
     let bReason = args.join(" ").slice(22);
     if(!bReason) return message.channel.send('Silahkan beri alasan');
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "MANAGE_MESSAGES");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send('Member ini tidak bisa di baned');
 
     let banEmbed = new Discord.MessageEmbed()
     .setDescription("~Ban~")
