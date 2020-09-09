61eeec255536a26c5d072d4b6dde57c7e3eaa704
Glitch (five-tricky-cardboard) <none>
2020-09-09 02:17:53 +0000

    🍿❄️ Checkpoint ./commands/userinfo.js:21161892/692

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -9,40 +9,41 @@ function checkDays(date) {
 exports.run = async (client, msg, args) => {
   let user = msg.mentions.users.first() || msg.author;
   let muser = msg.guild.member(msg.mentions.users.first()) || msg.member;
-  const embed = new Discord.MessageEmbed();
-  embed.addField("Username", `${user.tag}`, true)
+  const roles = muser.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString()).slice(0, -1);
+  const embed = new Discord.MessageEmbed()
+          .addField("Username", `${user.tag}`, true)
           .addField("ID", `${user.id}`, true)
           .setColor(3447003)
           .setThumbnail(user.avatarURL)
           .setTimestamp()
           .setURL(`${user.avatarURL}`)
-          .addField('Currently', `${muser.activity.status}`, true)
-          .addField('Game', `${muser.activity.game === null ? "No Game" : muser.activity.game.name}`, true)
+          .addField('Currently', `${muser.presence.status}`, true)
+          .addField('Game', `${muser.presence.game ? user.presence.game.name : 'None'}`, true)
           .addField('Joined Discord', `${moment(user.createdAt).toString().substr(0, 15)}\n(${moment(user.createdAt).fromNow()})`, true)
           .addField('Joined Server', `${moment(muser.joinedAt).toString().substr(0, 15)}\n(${moment(muser.joinedAt).fromNow()})`, true)
-          .addField('Roles', `${muser.roles.array()}`, true)
+          .addField('Roles', `${roles.length < 500 ? roles.join(', ') : roles.length > 500 ? this.client.utils.trimArray(roles) : 'None'}`, true)
           .addField('Is Bot', `${user.bot.toString().toUpperCase()}`, true)
           .setFooter(`univers cafe`, msg.guild.iconURL);
       msg.channel.send({embed});
 
-    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-    const mentionedUser = message.mentions.users.first() || message.author;
-    const roles = member.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString()).slice(0, -1);
-		const userFlags = mentionedUser.flags.toArray();
-    const date = moment(mentionedUser.createdAt).format('ll');
+//     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+//     const mentionedUser = message.mentions.users.first() || message.author;
+//     const roles = member.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString()).slice(0, -1);
+// 		const userFlags = mentionedUser.flags.toArray();
+//     const date = moment(mentionedUser.createdAt).format('ll');
     
-    const embed = new MessageEmbed()
-          .setThumbnail(mentionedUser.displayAvatarURL({ dynamic: true, size: 512 }))
-          .setColor('ff85cc')
-          .setFooter("Creator Beel")
-          .setTimestamp(message.createdTimestamp)
-          .addField('Username | Tags', `${mentionedUser.tag} | ${mentionedUser.discriminator}`)
-          .addField('ID', `${mentionedUser.id}`)
-          .addField('Badges', `${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`)
-          .addField('Join Date', `${moment(member.joinedAt).format('LL LTS')}`)
-          .addField('Created At', (date))
-          .addField('Roles', `${roles.length < 500 ? roles.join(', ') : roles.length > 500 ? this.client.utils.trimArray(roles) : 'None'}`) 
-      message.channel.send(embed)
+//     const embed = new MessageEmbed()
+//           .setThumbnail(mentionedUser.displayAvatarURL({ dynamic: true, size: 512 }))
+//           .setColor('ff85cc')
+//           .setFooter("Creator Beel")
+//           .setTimestamp(message.createdTimestamp)
+//           .addField('Username | Tags', `${mentionedUser.tag} | ${mentionedUser.discriminator}`)
+//           .addField('ID', `${mentionedUser.id}`)
+//           .addField('Badges', `${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`)
+//           .addField('Join Date', `${moment(member.joinedAt).format('LL LTS')}`)
+//           .addField('Created At', (date))
+//           .addField('Roles', `${roles.length < 500 ? roles.join(', ') : roles.length > 500 ? this.client.utils.trimArray(roles) : 'None'}`) 
+//       message.channel.send(embed)
 }
 exports.conf = {
   enabled: true,
