67ccb24d037f5c75edb208478439b510aab7db00
Glitch (five-tricky-cardboard) <none>
2020-09-12 08:33:02 +0000

    🚥⛄️ Checkpoint ./commands/addrole.js:21161892/1230 ./commands/say.js:21161892/701

--- commands/say.js
+++ commands/say.js
@@ -1,11 +1,17 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
-
+const botS = require('../botsettings.json')
+const prefix = botS.prefix
 module.exports.run = async (bot, message, args) => {
 
   message.delete();
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  let embed = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setTitle('Say Commands')
+  .setDescription(`**<usage>: \`${prefix}say [message]\`\n<example>: \`${prefix}say halo\`**`)
   let botmessage = args.join(" ");
+  if(!botmessage) return message.channel.send(embed)
   message.channel.send(botmessage);
 }
 
