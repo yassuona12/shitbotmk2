dc7770c748d070baa64db7418eda11eee7d302ad
Glitch (five-tricky-cardboard) <none>
2020-09-20 10:03:58 +0000

    🐉🎑 Checkpoint ./commands/announce.js:21161892/1273

--- commands/announce.js
+++ commands/announce.js
@@ -1,21 +1,31 @@
 const Discord = require('discord.js')
 exports.run = async (message, client, args) => {
-  let data = args.join(' ')
-  let rest_of_the_string = message.content.slice(data.length); //removes the first part
-    let array_of_arguments = rest_of_the_string.split(','); //[title, description, link, image]
+    let channel = message.mentions.channels.first()
+    if(!channel) {
+      return message.channel.send('Silahkan mention channel yang diinginkan')
+    }
+    let data = args.slice(1).join(' ')
+    let content = data.split(',.'); //[title, description, link, image]
+    let announce = new Discord.MessageEmbed()
+    .setColor(0x00ff99)
+    .setAuthor()
+    .setTitle()
+    .setDescription()
+    .setTimestamp()
+    .setFooter('Announcement Japanisme')
+    channel.send(announce)
+//     let embed = new Discord.MessageEmbed()
+//       .setColor(0x00AE86)
+//       .setTitle(array_of_arguments[0])
+//       .setDescription(array_of_arguments[1])
+//       .setImage(array_of_arguments[3])
+//       .setURL(array_of_arguments[2])
+//       .addField("Text", true)
+//       .setThumbnail("https://i.imgur.com/Rmiwd1j.png")
+//       .setFooter("Footer", array_of_arguments[3])
+//       .setTimestamp();
 
-    let embed = new Discord.MessageEmbed()
-      .setColor(0x00AE86)
-      .setTitle(array_of_arguments[0])
-      .setDescription(array_of_arguments[1])
-      .setImage(array_of_arguments[3])
-      .setURL(array_of_arguments[2])
-      .addField("Text", true)
-      .setThumbnail("https://i.imgur.com/Rmiwd1j.png")
-      .setFooter("Footer", array_of_arguments[3])
-      .setTimestamp();
-
-    message.channel.send({ embed });
+//     message.channel.send(embed);
 }
 module.exports.help = {
   name: "announce"
