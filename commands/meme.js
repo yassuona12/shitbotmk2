dbc75f71de2199ed8daf81fd0ef3ef8aef2f3b79
Glitch (five-tricky-cardboard) <none>
2020-09-20 12:01:58 +0000

    🚍🤘 Checkpoint ./commands/meme.js:9957624/1255 ./package.json:9957624/1752

---
+++ commands/meme.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const got = require('got');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed();
+    got('https://www.reddit.com/r/memes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+        embed.setTitle(`${memeTitle}`);
+        embed.setURL(`${memeUrl}`)
+        embed.setColor('RANDOM')
+        embed.setImage(memeImage);
+        embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+        embed.setTimestamp()
+        message.channel.send(embed)
+    }).catch(console.error);
+}
+
+module.exports.help = {
+    name: "meme",
+    description : "mememememememe",
+  usage : "meme"
+}
