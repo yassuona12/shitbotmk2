b06fd6905f65e6c9cc7597cba2fe6429da1ca76b
Glitch (five-tricky-cardboard) <none>
2020-09-21 07:24:02 +0000

    🍯🚀 Checkpoint ./commands/announce.js:9957624/2:21161892/681 ./commands/meme.js:21161892/54 ./server.js:21161892/45

--- commands/announce.js
+++ commands/announce.js
@@ -1,15 +1,26 @@
 const Discord = require('discord.js')
 exports.run = async (message, client, args) => {
-    let data = args.join(' ')
-    let content = data.split(',.') //[title, description, link, image]
-    let announce = new Discord.MessageEmbed()
-    .setColor(0x00ff99)
-    .setAuthor(data)
-    .setTitle()
-    .setDescription()
-    .setTimestamp()
-    .setFooter('Announcement Japanisme')
-message.channel.send(data);
+ // if (!message.member.hasPermission("ADMINISTRATOR")) return;
+  let before = args.slice(1).join(' ')
+  let after = before.split('/+')
+  let embed = new Discord.MessageEmbed()
+  .setAuthor('Japanese Announcement')
+  .setColor('#FFFF00')
+  .setDescription(after[0])
+  .setImage(after[1] || null)
+  let channel = message.mentions.channels.first()
+  if(!channel) return message.reply('Mention Channel terlebih dahulu')
+  channel.send(embed)
+//     let data = args.join(' ')
+//     let content = data.split(',.') //[title, description, link, image]
+//     let announce = new Discord.MessageEmbed()
+//     .setColor(0x00ff99)
+//     .setAuthor(data)
+//     .setTitle()
+//     .setDescription()
+//     .setTimestamp()
+//     .setFooter('Announcement Japanisme')
+// message.channel.send(data);
 }
 module.exports.help = {
   name: "announce"
