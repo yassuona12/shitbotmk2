530fe0aa0b9f02979e50ecc4913ed0b6c670a366
Glitch (five-tricky-cardboard) <none>
2020-09-21 13:03:13 +0000

    📖🚝 Checkpoint ./server.js:21161892/15173

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ bot.on("ready", async message => {
     console.log(e.stack);
   }
 });
+bot.login(process.env.TOKEN).catch(console.error);
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~START COMMANDS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
 bot.on("message", async message => {
   if (message.author.bot) return;
@@ -203,4 +204,22 @@ bot.on("message", message => {
   }
 });
 
-bot.login(process.env.TOKEN).catch(console.error);
+
+const embed = new Discord.MessageEmbed()
+    .setTitle('Some Title')
+    .setColor('#0099ff');
+
+bot.once('ready', async () => {
+    const channel = client.channels.cache.get('662913739691130929'); //This is the problem.
+    try {
+        const webhooks = await channel.fetchWebhooks(); //This will not execute because channel is undefined.
+        const webhook = webhooks.first();
+
+        await webhook.send('Webhook test', {
+            username: 'some-username',
+            embeds: [embed],
+        });
+    } catch (error) {
+        console.error('Error trying to send: ', error);
+    }
+});
