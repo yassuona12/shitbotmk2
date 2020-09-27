b480e50a26f468a5b2ebd6210544c58ec0f47b88
Glitch (five-tricky-cardboard) <none>
2020-09-27 14:54:02 +0000

    🐚🚥 Checkpoint ./commands/botdev.js:21161892/344

--- commands/botdev.js
+++ commands/botdev.js
@@ -1,2 +1,11 @@
 const Discord = require('discord.js')
-exports.run = async 
+exports.run = async (client, message, args) => {
+  let embed = new Discord.MessageEmbed()
+  .setAuthor('Japanisme Bot Development')
+  .setDescription(`<@Rapentot> <@Orchit> `)
+  message.channel.send(embed)
+  }
+
+module.exports.help = {
+  name: 'botdev'
+ }
